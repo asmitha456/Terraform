8005b5f5f0394a61204d04ca3e19bf31f2e3394f
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 18:45:24 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
+#Outputs are only rendered when terraform applies your plan. Running terraform plan will not render outputs
+
 output "instance_public_ip"{
   value=aws_instance.output_demo.public_ip
 }
