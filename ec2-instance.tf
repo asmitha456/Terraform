1d563ae237538ef19be0b5a7e4bd292216a093ba
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 18:50:35 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,18 +1,5 @@
-Create an EC2 intance and then create IAM user and add administrative policies
-Connect EC2 instance and install terraform & AWS CLI then configure the AWS CONFIG, use IAM access keys
-sudo apt update -y
-sudo su
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
-sudo apt install wget unzip vim -y
-terraform -v (Terraform v1.13.5)
-1. Terraform init - download and configure plugin for provider "aws"
-2. terraform plan - generate and shoe execution plan
-3. terraform apply - build the infrastructure
-4. terraform validate - validate all terraform files in present working directory
-
 #Login to AWS EC2 console and observe an EC@ instance getting deployed
+
 provider "aws"{
         access_key=""
         secret_key=""
