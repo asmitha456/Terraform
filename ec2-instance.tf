1f3a205f70b79c1d9102e1e633686adb5866276f
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 18:24:32 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -26,11 +26,20 @@ resource "aws_instance" "Demo1"{
                 name="Demo1"
         }
 }
-If you dont want to write access keys in script then execute below commands
+
+#If you dont want to write access keys in script then execute below commands
+#Authentication using environment variables, it will work only current shell if you reboot again will need to export these variables again
   export aws_access_key_id=""
   export aws_secret_key=""
   export aws_default_region="us-east-1"
   terraform plan
 
+#AWS CLI Installation
+1. download file
+2. unzip file
+3. run install program
+4. check version aws --version
+5. aws configure to enter aws access keys
+6. aws credentials stored in sudo cat ~/.aws/credentals
 
 
