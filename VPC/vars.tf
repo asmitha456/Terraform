3c48b2f491409caf10e99dfa93f1d9adf2b39b3d
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 16:23:11 +0530

    Update vars.tf

--- VPC/vars.tf
+++ VPC/vars.tf
@@ -1,3 +1,19 @@
-variable "aws_region"{
+variable "aws-instance"{
+    instance_type="t3.micro"
+}
+
+varible "ami"{
+ ami=""
+}
+
+variable "aws_region"{    
     default="us-east-1"
+}
+
+variable "path_to_private_key"{
+    default="vpckeypair"
+}
+
+variable "path_to_public_key"{
+    default="vpckeypair.pub"
 }
