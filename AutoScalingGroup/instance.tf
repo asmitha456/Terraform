6811ffd9fde2a36b9f7652c2cb6d0890c5b0b356
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 17:25:05 +0530

    Create instance.tf

---
+++ AutoScalingGroup/instance.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "Demo"{
+  ami=""
+  instance_type="t3.micro"
+  vpc_security_group_ids=[aws_security_group.web-security.id]
+  user_data=<<-EOF
+    #!/bin/bash
+    echo "you are learning Terraform, Have a good day" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+  lifecycle{
+    create_before_destroy=true
+  }
+}
