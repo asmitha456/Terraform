535589893d7f2ef20b388802a05688fb7bcecdfb
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 13:33:22 +0530

    Create main.tf

---
+++ Storage/main.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "terraform_s3"{
+#S3 bucket name should be unique in a region
+bucket="terraform-demo3"
+
+#Prevent accidental deletion
+lifecycle{
+  prevent_destroy=true
+}
+
+#Enable Versioning
+versioning{
+  enabled=true
+}
+
+#Enabling server side encryption
+server_side_encryption_configuration{
+  rule{
+    apply_server_side_encryption_by_default{
+      sse_algorithm="AES256"
+     }
+   }
+ }
+}
