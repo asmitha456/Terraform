2a5d6cb485579415eeaa5c393d7dde3c7cfbdb46
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 13:45:59 +0530

    Update vars.tf

--- variables/vars.tf
+++ variables/vars.tf
@@ -11,3 +11,12 @@ variable "ami"{
   us-east-1="ami-0157af9aea2eef346"
   }
 }
+
+variable "path_to_private_key"{
+  default="myfirstkey"
+}
+variable "path_to_public_key"{
+  default="myfirstkey.pub"
+}
+#Generate a key pair usig ssh-keygen -f myfirstkey
+#Connect using prive key ssh -i myfirstkey ubuntu@public-ip of launched instance
