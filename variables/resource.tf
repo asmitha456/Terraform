5ad05b4908e7ca445c8e95e7b6c564e9cea9771b
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 13:40:02 +0530

    Update resource.tf

--- variables/resource.tf
+++ variables/resource.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Demo2"{
         ami=lookup(var.ami, var.aws_region)
         instance_type="t3.micro"
+        key_name="terraform"
         tags={
                 name="Demo2"
         }
