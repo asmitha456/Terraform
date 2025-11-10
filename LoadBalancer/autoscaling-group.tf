f8e2713471005821861fbe725073e30d28bd8f5b
asmitha456 <140152177+asmitha456@users.noreply.github.com>
2025-11-10 17:46:32 +0530

    Create autoscaling-group.tf

---
+++ LoadBalancer/autoscaling-group.tf
@@ -0,0 +1,21 @@
+resource "aws_autoscaling_group" "demo"{
+  launch_configuration=aws_launch_configuration.demo.name
+  vpc_zone_identifier=data.aws_subnet_ids.default.ids
+  
+  min_size=2
+  max_size=10
+
+  tag{
+    key="demo"
+    value="websevrer-with-asg-lb"
+    propagate_at_launch=true
+  }
+}
+
+data "aws_vpc" "default"{
+  default=true
+}
+
+data "aws_subnet_ids" "default"{
+  vpc_id=data.aws_vpc.default.id
+}
